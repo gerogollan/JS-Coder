e922888a0ae1209b2a64b3f1473de51e6ec23ac3
gerogollan <gerogollan@gmail.com>
2024-11-03 09:05:34 -0300

    finalizado

--- js/main.js
+++ js/main.js
@@ -1,138 +1,151 @@
-let transacciones = []; // Array para guardar las transacciones
 
-const form = document.querySelector("#form"); //constante que selecciona el formulario
-const montoInput = document.getElementById("formMonto"); //const para los datos del form, monto
-const descripcionInput = document.getElementById("descripción");//descripcion
-const tipoSelect = document.getElementById("tipo");//tipo de movimiento
+//codigo 6
+let transacciones = [];
 
-form.addEventListener("submit", function (event) { //evento
+const form = document.querySelector("#form");
+const montoInput = document.getElementById("formMonto");
+const descripcionInput = document.getElementById("descripción");
+const tipoSelect = document.getElementById("tipo");
+
+
+//submit
+form.addEventListener("submit", function (event) {
   event.preventDefault(); //sin esto se me reinicia la pagina
 
-  const monto = parseFloat(montoInput.value); //recibe el monto y lo transpasa como numero
-  const descripcion = descripcionInput.value.trim(); 
+  const monto = parseFloat(montoInput.value);
+  const descripcion = descripcionInput.value.trim();
   const tipo = tipoSelect.value;
 
-  // Valida que el usuario ingrese bien los datos
-  if (isNaN(monto) || monto <= 0) { //si isnotanumber(monto) o monto igual o menor a 0 tira el alert
-    alert("Por favor, ingresa un monto válido.");
+
+  //fecha del movimiento
+  const fecha = new Date();
+  const dia = fecha.getDate().toString().padStart(2, '0'); 
+  const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+
+
+  // Validación que el usuario ingrese bien los datos
+  if (isNaN(monto) || monto <= 0) {
+    Swal.fire({
+      icon: "question",
+      title: "Por favor",
+      text: "Ingresa un monto válido!",
+      timer: 1500,
+      showConfirmButton: false,
+    });
     return;
   }
-  if (descripcion === "") { //si la descripcion esta vacia te tira el alert
-    alert("La descripción no puede estar vacía.");
+  if (descripcion === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "La descripción no puede estar vacía!",
+      timer: 1500,
+      showConfirmButton: false,
+    });
     return;
+
   }
 
-  const transaccion = { tipo, monto, descripcion };
-  transacciones.push(transaccion); // Agrega la transacción al array
-  localStorage.setItem("transacciones", JSON.stringify(transacciones)); // Guarda en localStorage
+  
+
+  const transaccion = { tipo, monto, descripcion,fecha:`${dia}/${mes}` };
+  transacciones.push(transaccion); // Agrega la transacción al array del inicio
+  localStorage.setItem("transacciones", JSON.stringify(transacciones));
   renderizarTransacciones();
   calcularSaldo();
-  form.reset(); // resetea el formulario
+  actualizarSaldoUSD();
+  form.reset();
 });
 
-// recarga las transacciones cuando abrimos la pagina
+// Carga las transacciones cuando abrimos la pagina
 if (localStorage.getItem("transacciones")) {
   transacciones = JSON.parse(localStorage.getItem("transacciones")); //trae las transacciones guardadas
 }
 
-// muestra las transacciones
-function renderizarTransacciones(filtro = "todos") {
-  const listaTransacciones = document.querySelector("#transacciones");
-  listaTransacciones.innerHTML = ""; // Limpia la lista antes de renderizar
-
-  transacciones.forEach((transaccion, index) => {
-    if (filtro === "todos" || transaccion.tipo.toLowerCase() === filtro) {
-      const li = document.createElement("li");
-      li.textContent = `${transaccion.descripcion}: ${transaccion.monto}`;
-      li.className =
-        transaccion.tipo.toLowerCase() === "ingreso" ? "ingreso" : "gasto";
-
-      //boton para eliminar
-      const btnEliminar = document.createElement("button"); //crea el button
-      btnEliminar.textContent = "Eliminar"; //este es el texto del button
-      btnEliminar.addEventListener("click", () => eliminarTransaccion(index));
-      li.appendChild(btnEliminar);
-
-      //button para editar
-      const btnEditar = document.createElement("button");
-      btnEditar.textContent = "Editar";
-      btnEditar.addEventListener("click", () => editarTransaccion(index)); //evento para editar
-      li.appendChild(btnEditar);
-
-      listaTransacciones.appendChild(li); // Agrega el nuevo elemento a la lista
-    }
-  });
-}
-
-//calcular y mostrar saldo
+// Calcular y mostrar saldo
 function calcularSaldo() {
   const totalIngresos = transacciones.reduce(
     (sum, transaccion) =>
-      transaccion.tipo === "Ingreso" ? sum + transaccion.monto : sum,
+      transaccion.tipo.toLowerCase() === "ingreso"
+        ? sum + transaccion.monto
+        : sum,
     0
   );
   const totalGastos = transacciones.reduce(
     (sum, transaccion) =>
-      transaccion.tipo === "Gasto" ? sum + transaccion.monto : sum,
+      transaccion.tipo.toLowerCase() === "gasto"
+        ? sum + transaccion.monto
+        : sum,
     0
   );
+
   const saldo = totalIngresos - totalGastos;
-  if (saldo < 0) { //if(SI) saldo < 0 genera un alert
-    alert("El Saldo de tu cuenta no puede ser un numero negativo");
+  
+
+  if (saldo < 0) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El saldo de tu cuenta no puede ser un número negativo.",
+      timer: 1500,
+      showConfirmButton: false,
+    });
+    return 0; // Devuelve 0 si el saldo es negativo
+  
   }
-  document.querySelector("#saldo").textContent = saldo;
+
+  document.querySelector("#saldo").textContent = "$ " + saldo;
+  return saldo;
 }
 
-//function para eliminar transacción
+
+// Function para eliminar transacción
 function eliminarTransaccion(index) {
   transacciones.splice(index, 1); //elimina la transacción del array
-  localStorage.setItem("transacciones", JSON.stringify(transacciones)); //actualiza localStorage
-  renderizarTransacciones(); //vuelve a mostrar
-  calcularSaldo(); //actualiza el saldoz
+  localStorage.setItem("transacciones", JSON.stringify(transacciones));
+  renderizarTransacciones(); 
+  calcularSaldo(); 
+  actualizarSaldoUSD(); 
 }
 
-//funcion para editar transacciónes
-function editarTransaccion(index) {
-  montoInput.value = transacciones[index].monto; //rellena el monto
-  descripcionInput.value = transacciones[index].descripcion; //rellena ladescripción
-  tipoSelect.value = transacciones[index].tipo; //
-  eliminarTransaccion(index); //elimina la transacción para que se agregue de nuevo al guardar
-}
-
-//Historial
-document.querySelector("#toggleHistorial").addEventListener("click", function () {
-    const historial = document.querySelector("#transacciones"); //selecciona el historial
-    //constante nombreconstante = document(html).queryselector(seleccionador)("#nombre")
-
+// HISTORIAL
+document
+  .querySelector("#toggleHistorial")
+  .addEventListener("click", function () {
+    const historial = document.querySelector("#transacciones");
+    const filtroSelect = document.querySelector("select#filtro");
     const mostrando =
       historial.style.display === "none" || historial.style.display === "";
-      //verificacion para ver si esta oculto el historial
 
-    historial.style.display =
-      historial.style.display === "none" ? "block" : "none";
-      historialText.style.display === "none" ? "block" : "none";
-      //historial > estilo > estilo display 
+    historial.style.display = mostrando ? "block" : "none";
+    filtroSelect.style.display = mostrando ? "block" : "none";
 
     if (mostrando) {
       renderizarHistorial();
     }
   });
 
-//renderizar el historial
-function renderizarHistorial() {
-  const historial = document.querySelector("#transacciones");
-  historial.innerHTML = ""; //esto limpia el historial antes de renderizar
 
-const h4 = document.createElement("h4"); //crea un elemento h4
-h4.textContent = `Ultimos Movimientos`; // el texto del h4
-historial.appendChild(h4); //agrega el h4 a #transacciones > historial
+//funcion renderizar historial
+function renderizarTransacciones(filtro = "todos") {
+  const listaTransacciones = document.querySelector("#transacciones");
+  listaTransacciones.innerHTML = "";
 
-  transacciones.forEach((transaccion) => {
-    const li = document.createElement("li"); //crea un li
-    li.textContent = `${transaccion.descripcion}: ${transaccion.monto} (${transaccion.tipo})`; 
-    //li.texcontent(el texto del li) = `$(variabletransaccion.valordescripcion)`
+  transacciones.forEach((transaccion, index) => {
+    if (filtro === "todos" || transaccion.tipo.toLowerCase() === filtro) {
+      const li = document.createElement("li");
+      li.textContent = `${transaccion.fecha} | ${transaccion.descripcion.toUpperCase()} : ${transaccion.monto}`;
 
-    historial.appendChild(li); //lo sube como hijo al historial html
+      li.className =
+        transaccion.tipo.toLowerCase() === "ingreso" ? "ingreso" : "gasto";
+
+      const btnEliminar = document.createElement("button");
+      btnEliminar.textContent = "Eliminar";
+      btnEliminar.addEventListener("click", () => eliminarTransaccion(index));
+      li.appendChild(btnEliminar);
+
+      listaTransacciones.appendChild(li);
+    }
   });
 }
 
@@ -144,4 +157,96 @@ filtroSelect.addEventListener("change", function () {
 
 //al cargar la página, renderiza transacciones
 renderizarTransacciones(); //esto muestra/ renderiza las transacciones
-calcularSaldo(); //calcula el saldo cada vez que se ejecuta la pagina.
+calcularSaldo(); //calcula el saldo cada vez que se ejecuta la pagina
+
+
+
+//equivalente en usd
+async function getPrecioDolar() {
+  let precioDolar = 0;
+
+  try {
+    const response = await fetch("https://dolarapi.com/v1/dolares/blue");
+    if (!response.ok) {
+      throw new Error(`Caída en la red: ${response.status}`);
+    }
+
+    const data = await response.json();
+    precioDolar = data.venta;
+
+    return precioDolar;
+
+  } catch (err) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'No se pudo obtener el precio del dólar.',
+      confirmButtonText: "Entendido"
+  });
+  }
+}
+
+// Función para actualizar el saldo en USD
+async function actualizarSaldoUSD() {
+  const saldoActual = calcularSaldo();
+  const precioDolar = await getPrecioDolar();
+  if (precioDolar) {
+    const saldoUsd = saldoActual / precioDolar;
+    let h3Saldo = document.querySelector("#saldoUsd");
+    h3Saldo.innerText = `Equivalente en USD (Dólar Blue): $ ${saldoUsd.toFixed(2)}`;
+  }
+}
+
+actualizarSaldoUSD();
+
+
+
+//usd compra y venta 
+async function obtenerValoresDolar() {
+  try {
+    const response = await fetch("https://dolarapi.com/v1/dolares/blue");
+    
+    if (!response.ok) {
+      throw new Error(`Error en la API: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    
+
+    if (data.compra && data.venta) {
+      document.getElementById("compraDolar").textContent = data.compra.toFixed(2);
+      document.getElementById("ventaDolar").textContent = data.venta.toFixed(2);
+      
+    } else {
+       }
+  } catch (err) {
+    console.error("Error cuando se intento obtener el valor del dólar:", err);
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'No se pudo obtener el precio del dólar.',
+      confirmButtonText: "Entendido"
+    });
+  }
+}
+
+// Llama a la función para obtener y mostrar los valores del dólar en la carga de la página
+obtenerValoresDolar();
+
+
+// Llama a la función para obtener y mostrar los valores del dólar en la carga de la página
+obtenerValoresDolar();
+
+//resumen
+document.getElementById("resumenCuenta").addEventListener("click", () => {
+  if (localStorage.getItem("sesionIniciada") === "true") {
+      window.location.href = "resumen.html"; 
+  } else {
+      Swal.fire({
+          icon: "warning",
+          title: "No estás logueado",
+          text: "Por favor, inicia sesión para acceder al resumen de cuenta.",
+      });
+  }
+});
